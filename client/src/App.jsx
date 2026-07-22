7ad9894122656c10924c03c78ceba553444aba6a
Vishnu <vishnusvprasad@gmail.com>
2026-07-22 17:32:12 +0530

    added prebuild page, components page, Detail page, footer and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-
+import Prebuild from './pages/Prebuild';
 import Footer from './components/Footer';
+
+import Detail from './pages/Detail';
+import ComponentsCatalog from './pages/ComponentsCatalog';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col">
-      <Navbar />
-      <div className="flex-grow">
-        <Home />
+    <BrowserRouter>
+      <div className="min-h-screen bg-black text-white flex flex-col">
+        <Navbar />
+        <div className="flex-grow bg-white">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/prebuild" element={<Prebuild />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/components/:category" element={<ComponentsCatalog />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
