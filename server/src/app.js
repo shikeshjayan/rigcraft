eb38d81aea6f73135ec069ceb5d4abc0ef669536
shikeshjayan <shikeshjayan345@gmail.com>
2026-07-28 13:00:20 +0530

    fix: move dotenv.config() to app.js to fix CORS preflight origin loading
    Moved dotenv.config() from server.js to app.js so environment variables
    (especially CORS_ORIGIN) are available before Express middleware config
    evaluates, fixing the blocked preflight on login. ESM evaluates static
    imports before the importing module's body, so app.js modules ran before
    dotenv loaded .env vars in server.js.

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import dns from "dns";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import http from "http";
 import connectDB from "./config/db.js";
 import app from "./app.js";
