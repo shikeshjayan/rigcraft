2923de709ee09ed12bb795d0b91041025d61ab23
shikeshjayan <shikeshjayan345@gmail.com>
2026-08-03 20:45:23 +0530

    fix: herobrand.jsx,settings.service.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
       "/uploads": {
         target: "http://localhost:5000",
         changeOrigin: true,
